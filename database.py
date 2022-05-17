c07560cc424f9b6a8f3a7ca71964c14307799a4b
Mahmoud Hussein <mahmoudh3257@gmail.com>
2022-05-17 22:07:20 +0200

    Fix select method

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 import sqlite3
 from sqlite3 import Error
 
+
 class Database:
 
     def __init__(self, db_file):
-        self.db_file = db_file # Path to database file.
+        self.db_file = db_file  # Path to database file.
 
     def _connect_db(self):
         """ Make a connection to the SQLite database. """
@@ -13,16 +14,16 @@ class Database:
             conn = sqlite3.connect(self.db_file)
         except Error as e:
             print(e)
-        
+
         return conn
 
     def _close_db(self, conn):
         """ Close connection to database. """
         if conn:
             conn.close()
-        
-    def select(self, table, id):
-        """ Return the row specified by the id from a certain table. """
+
+    def select(self, id: int) -> tuple:
+        """ Return the row specified by the id.. """
         # Create connection to db.
         conn = self._connect_db()
 
@@ -30,8 +31,6 @@ class Database:
         cur = conn.cursor()
 
         # Execute query.
-        cur.execute("SELECT * FROM ? WHERE id=?", (table, id))
-
-        row = cur.fetchall()
+        row = cur.execute("SELECT * FROM Users WHERE id=?", (id,)).fetchall()[0]
 
         return row
