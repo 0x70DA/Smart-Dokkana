a622e6b122427ef267c7c0564bc18c30af44b5c9
Mahmoud Hussein <mahmoudh3257@gmail.com>
2022-05-18 11:34:17 +0200

    Integrate database with server

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, session
 from flask_session import Session
+from database import Database
 
 app = Flask(__name__)
 
@@ -9,38 +10,49 @@ app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 # Temp dictionary to hold usernames and passwords. To be replaced with a SQL database.
-USERS = {'Mahmoud Hussein': 'admin1234', 'Test': 'Test'}
+# USERS = {'Mahmoud Hussein': 'admin1234', 'Test': 'Test'}
+
+# Configure database.
+db_file = "database.db"
+db = Database(db_file)
 
 
 @app.route('/')
 def index():
     '''Define the default route.'''
     # If no user is logged in, redirect to login page.
-    if not session.get('username'):
+    if not session.get('id'):
         return redirect('/login')
 
-    return render_template('index.html')
+    # Get user's data.
+    user_data = db.select(session.get('id'))
+    
+    return render_template('index.html', user_data=user_data)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     '''Define the login route.'''
-    username = request.form.get('username')
-    password = request.form.get('password')
     if request.method == 'POST':
         # User submitted a form.
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Get all rows from db.
+        rows = db.select_all()
+
         # If user is in database, log them in.
-        for user in USERS:
-            if username == user and password == USERS[user]:
+        for row in rows:
+            if username == row['username'] and password == row['password']:
                 # Add user to session and redirect to homepage.
-                session['username'] = username
+                session['id'] = row['id']
                 return redirect('/')
 
         # User is not registered.
         return render_template('login.html', msg='fail')
 
     # GET request.
-    if not session.get('username'):  # If user is not logged in.
+    if not session.get('id'):  # If user is not logged in.
         return render_template('login.html')
     else:
         # User already logged in.
@@ -51,7 +63,7 @@ def login():
 def logout():
     '''Define logout route.'''
     # Remove user from session.
-    session['username'] = None
+    session['id'] = None
     return redirect('/')
 
 
